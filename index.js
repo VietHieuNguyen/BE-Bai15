17cc3dafe24ba8ec5b0ad0fa72d3464f394d66a0
JustHieu <hieusanta456@gmail.com>
2025-11-15 00:25:52 +0700

    install mongoose

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+
 require("dotenv").config();
+
+
 
 const app = express();
 const port = process.env.PORT;
+const database = require("./config/database")
+
+database.connect();
+
 const route = require("./routes/client/index.route");
+
+
 
 app.set("views","./views");
 app.set("view engine","pug");
