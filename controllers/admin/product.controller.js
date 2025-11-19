34423b150d5cdebcafc58cae7d894dab6267d02a
JustHieu <hieusanta456@gmail.com>
2025-11-19 16:29:22 +0700

    B17 3 4.1. Xóa vĩnh viễn

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,55 +1,52 @@
 // [GET] /admin/products
-const Product = require("../../models/product.model")
+const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
-const searchHelper = require("../../helpers/search")
-const paginationHelper = require("../../helpers/pagination")
+const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 
-
-module.exports.index = async(req,res) =>{
-  
+module.exports.index = async (req, res) => {
   const filterStatus = filterStatusHelper(req.query);
   // console.log(filterStatus)
-  let find={
+  let find = {
     deleted: false,
-  }
-  if(req.query.status){
+  };
+  if (req.query.status) {
     find.status = req.query.status;
   }
 
   const objectSearch = searchHelper(req.query);
-  if(objectSearch.regex){
+  if (objectSearch.regex) {
     find.title = objectSearch.regex;
   }
-// Pagination
-  const countProducts = await Product.countDocuments(find)
+  // Pagination
+  const countProducts = await Product.countDocuments(find);
 
   let objectPagination = paginationHelper(
     {
-    currentPage: 1,
-    litmitItems:4
-  },
-  req.query,
-  countProducts)
+      currentPage: 1,
+      litmitItems: 4,
+    },
+    req.query,
+    countProducts
+  );
 
-  
+  // End Pagination
 
-
-// End Pagination 
-
-
-  const products = await Product.find(find).limit(objectPagination.litmitItems).skip(objectPagination.skip)
+  const products = await Product.find(find)
+    .limit(objectPagination.litmitItems)
+    .skip(objectPagination.skip);
 
   // console.log(products)
 
-  res.render("admin/pages/products/index",{
-    pageTitle:"Danh sách sản phẩm",
+  res.render("admin/pages/products/index", {
+    pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
     keyword: objectSearch.keyword,
-    pagination: objectPagination
-  })
-}
+    pagination: objectPagination,
+  });
+};
 
 // [PATCH] /admin/products/change-status/:status/:id
 
@@ -60,34 +57,43 @@ module.exports.changeStatus = async (req, res) => {
   const allowed = ["active", "inactive"];
 
   if (!allowed.includes(status) || !mongoose.isValidObjectId(id)) {
-     res.redirect("back")
+    res.redirect("back");
   }
 
   await Product.updateOne({ _id: id }, { status });
 
-  const redirectUrl = req.get("Referrer") || req.app.locals.prefixAdmin + "/products";
+  const redirectUrl =
+    req.get("Referrer") || req.app.locals.prefixAdmin + "/products";
   res.redirect(redirectUrl);
 };
-// [GET] /admin/products/change-status/:status/:id
-
+// [PATCH] /admin/products/change-status/:status/:id
 
 module.exports.changeMulti = async (req, res) => {
-  
   const type = req.body.type;
   const ids = req.body.ids.split(", ");
 
-  switch (type){
+  switch (type) {
     case "active":
-      await Product.updateMany({_id: {$in : ids}},{status: "active"})
+      await Product.updateMany({ _id: { $in: ids } }, { status: "active" });
       break;
     case "inactive":
-      await Product.updateMany({_id: {$in : ids}},{status: "inactive"})
+      await Product.updateMany({ _id: { $in: ids } }, { status: "inactive" });
 
       break;
     default:
       break;
   }
-  const redirectUrl = req.get("Referrer") || req.app.locals.prefixAdmin + "/products";
+  const redirectUrl =
+    req.get("Referrer") || req.app.locals.prefixAdmin + "/products";
   res.redirect(redirectUrl);
-
 };
+
+// [DELETE] /admin/prodcuts/delete/:id
+module.exports.deleteItem = async(req,res) =>{
+  const id = req.params.id;
+
+  await Product.deleteOne({_id:id})
+  const redirectUrl =
+    req.get("Referrer") || req.app.locals.prefixAdmin + "/products";
+  res.redirect(redirectUrl);
+}
--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -1,22 +1,45 @@
 // change status
-const butonsChangeStatus = document.querySelectorAll("[button-change-status]")
-if(butonsChangeStatus.length > 0){
-  const formChangeStatus = document.querySelector("#form-change-status")
-  const path = formChangeStatus.getAttribute("data-path")
+const butonsChangeStatus = document.querySelectorAll("[button-change-status]");
+if (butonsChangeStatus.length > 0) {
+  const formChangeStatus = document.querySelector("#form-change-status");
+  const path = formChangeStatus.getAttribute("data-path");
 
-  butonsChangeStatus.forEach(button =>{
-    button.addEventListener("click",()=>{
-      const statusCurrent = button.getAttribute("data-status")
-      const id = button.getAttribute("data-id")
-      if(!id) return
+  butonsChangeStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const statusCurrent = button.getAttribute("data-status");
+      const id = button.getAttribute("data-id");
+      if (!id) return;
       let statusChange = statusCurrent == "active" ? "inactive" : "active";
-      
-      const action = path + `/${statusChange}/${id}?_method=PATCH`
+
+      const action = path + `/${statusChange}/${id}?_method=PATCH`;
       // console.log({ path, statusChange, id, action });
-      formChangeStatus.action = action
+      formChangeStatus.action = action;
 
       formChangeStatus.submit();
-    })
-  })
+    });
+  });
 }
 // end change status
+
+// Delete item
+const buttonsDelete = document.querySelectorAll("[button-delete]");
+const formDeleteItem = document.querySelector("#form-delete-item");
+const path = formDeleteItem.getAttribute("data-path")
+
+if (buttonsDelete.length > 0) {
+  buttonsDelete.forEach((button) => {
+    button.addEventListener("click", () => {
+      const isConfirm = confirm("Bạn có chắc muốn xóa không");
+      if (isConfirm) {
+        const id = button.getAttribute("data-id");
+        const action = `${path}/${id}?_method=DELETE`;
+
+        console.log(action)
+        formDeleteItem.action = action;
+        formDeleteItem.submit();
+      }
+    });
+  });
+}
+
+// END Delete item
