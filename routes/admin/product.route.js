fd48a36a3286c1008a86c44f75baa5197af90433
JustHieu <hieusanta456@gmail.com>
2025-11-30 13:24:14 +0700

    B20 1 1. Sắp xếp sản phẩm theo các tiêu chí khác nhau

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -31,10 +31,13 @@ router.get("/edit/:id",controller.edit)
 router.patch(
   "/edit/:id",
   upload.single("thumbnail"),
+  uploadCloud.upload,
   validate.createPost,
   controller.editPatch)
 
 router.get("/detail/:id",controller.detail)
+
+
 
 
 module.exports = router;
