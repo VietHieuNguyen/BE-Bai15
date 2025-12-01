002c102668f611ad9d5acf5477c0205115220c3e
JustHieu <hieusanta456@gmail.com>
2025-12-02 00:26:55 +0700

    B20 3 3. Xây dựng phần quản lí danh mục sản phẩm (Phần 2)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,29 +1,17 @@
 const ProductCategory = require("../../models/product-category.model")
 const systemConfig = require("../../config/system")
 
+
+const createTreeHelper = require("../../helpers/createtTree")
 // [GET] /admin/products-category
 module.exports.index = async (req, res) => {
    let find = {
     deleted: false,
   };
-  function createTree(arr,parentId = ""){
-    const tree = []
-    arr.forEach((item) => {
-      if(item.parent_id == parentId){
-        const newItem = item;
-        const children = createTree(arr, item.id);
-        if(children.length > 0){
-          newItem.children = children;
-        }
-        tree.push(newItem)
-      }
-    });
-    return tree;
-  }
-  const records = await ProductCategory.find(find);
-
-  const newRecords = createTree(records);
-  console.log(newRecords)
+  
+  const records = await ProductCategory.find(find)
+  const newRecords = createTreeHelper.tree(records);
+  
   res.render("admin/pages/products-category/index", {
     pageTitle: "Danh mục sản phẩm",
     records: newRecords
@@ -36,10 +24,13 @@ module.exports.create = async (req, res) => {
   let find = {
     deleted:false,
   }
+
+  
   const records = await ProductCategory.find(find);
+  const newRecords = createTreeHelper.tree(records);
   res.render("admin/pages/products-category/create", {
     pageTitle: "Danh mục sản phẩm",
-    records: records
+    records:newRecords
   });
 };
 //[POST] /admin/products/create
