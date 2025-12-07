0604e31d716a20b010df999988f2145ac8930ea2
JustHieu <hieusanta456@gmail.com>
2025-12-07 18:29:40 +0700

    B21 1 1.Tính năng chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,48 +1,81 @@
-const ProductCategory = require("../../models/product-category.model")
-const systemConfig = require("../../config/system")
+const ProductCategory = require("../../models/product-category.model");
+const systemConfig = require("../../config/system");
 
-
-const createTreeHelper = require("../../helpers/createtTree")
+const createTreeHelper = require("../../helpers/createtTree");
 // [GET] /admin/products-category
 module.exports.index = async (req, res) => {
-   let find = {
+  let find = {
     deleted: false,
   };
-  
-  const records = await ProductCategory.find(find)
+
+  const records = await ProductCategory.find(find);
   const newRecords = createTreeHelper.tree(records);
-  
+
   res.render("admin/pages/products-category/index", {
     pageTitle: "Danh mục sản phẩm",
-    records: newRecords
-    
+    records: newRecords,
   });
 };
 // [GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
-
   let find = {
-    deleted:false,
-  }
+    deleted: false,
+  };
 
-  
   const records = await ProductCategory.find(find);
   const newRecords = createTreeHelper.tree(records);
   res.render("admin/pages/products-category/create", {
     pageTitle: "Danh mục sản phẩm",
-    records:newRecords
+    records: newRecords,
   });
 };
-//[POST] /admin/products/create
-module.exports.createPost= async(req,res)=>{
-   if(req.body.position == ""){
+//[POST] /admin/products-category/create
+module.exports.createPost = async (req, res) => {
+  if (req.body.position == "") {
     const count = await ProductCategory.countDocuments();
     req.body.position = count + 1;
-  }else{
-    req.body.position = parseInt(req.body.position)
-
+  } else {
+    req.body.position = parseInt(req.body.position);
   }
   const record = new ProductCategory(req.body);
-  await record.save()
-  res.redirect(`${systemConfig.prefixAdmin}/products-category`)
+  await record.save();
+  res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+};
+//[GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await ProductCategory.findOne({
+      _id: id,
+      deleted: false,
+    });
+    const records = await ProductCategory.find({
+      deleted: false,
+    });
+    const newRecords = createTreeHelper.tree(records);
+    res.render("admin/pages/products-category/edit", {
+      pageTitle: "Trang chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+  }
+};
+
+//[PATCH] /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  req.body.position = parseInt(req.body.position);
+
+  await ProductCategory.updateOne(
+    {
+      _id: id,
+    },
+    req.body
+  );
+
+  const redirectUrl =
+    req.get("Referrer") || req.app.locals.prefixAdmin + "/products";
+  res.redirect(redirectUrl);
 };
--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -16,8 +16,16 @@ router.post(
   upload.single("thumbnail"),
   uploadCloud.upload,
   validate.createPost,
-  controller.createPost
+  controller.editPatch
 );
 
+router.get("/edit/:id",controller.edit)
+router.patch(
+  "/edit/:id",
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.editPatch
+);
 
 module.exports = router;
