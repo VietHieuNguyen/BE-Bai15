3cf2fb50c0ba310641fd2968fcd199d71a39a700
JustHieu <hieusanta456@gmail.com>
2025-12-07 19:23:05 +0700

    B21 1 2.1 Phần thêm mới sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,9 +1,12 @@
 
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
+
+const createTreeHelper = require("../../helpers/createtTree")
 
 const systemConfig = require("../../config/system")
 // [GET] /admin/products
@@ -135,8 +138,14 @@ module.exports.deleteItem = async(req,res) =>{
 
 //[GET] /admin/products/create
 module.exports.create = async(req,res)=>{
+    let find = {
+      deleted: false
+    };
+    const category = await ProductCategory.find(find)
+    const newCategory = createTreeHelper.tree(category);
     res.render("admin/pages/products/create", {
     pageTitle: "Thêm mới sản phẩm",
+    category: newCategory
     
   });
 };
