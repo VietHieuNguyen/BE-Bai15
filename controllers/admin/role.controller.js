b794716c86d096089746762d428be65c3ee402f0
JustHieu <hieusanta456@gmail.com>
2025-12-08 17:39:22 +0700

    B21 1 3.4 Xây dựng phần chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,36 +1,69 @@
-const Role = require("../../models/role.model")
-const systemConfig = require("../../config/system")
+const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
 
 // [GET] /admin/roles
 
 module.exports.index = async (req, res) => {
   let find = {
-    deleted:false
-  }
-  const records = await Role.find(find)
-   res.render("admin/pages/roles/index",{
-      pageTitle: "Trang nhóm quyền",
-      records: records
-    });
-  }
+    deleted: false,
+  };
+  const records = await Role.find(find);
+  res.render("admin/pages/roles/index", {
+    pageTitle: "Trang nhóm quyền",
+    records: records,
+  });
+};
 
-  // [GET] /admin/roles/create
+// [GET] /admin/roles/create
 
 module.exports.create = async (req, res) => {
   let find = {
-    deleted:false
-  }
-  const records = await Role.find(find)
-   res.render("admin/pages/roles/create",{
-      pageTitle: "Tạo nhóm quyền"
-    });
-  }
+    deleted: false,
+  };
+  const records = await Role.find(find);
+  res.render("admin/pages/roles/create", {
+    pageTitle: "Tạo nhóm quyền",
+  });
+};
 
-  // [POST] /admin/roles/create
+// [POST] /admin/roles/create
 
 module.exports.createPost = async (req, res) => {
-   console.log(req.body)
-   const record = new Role(req.body);
-   await record.save();
-   res.redirect(`${systemConfig.prefixAdmin}/roles`)
+  console.log(req.body);
+  const record = new Role(req.body);
+  await record.save();
+  res.redirect(`${systemConfig.prefixAdmin}/roles`);
+};
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    let find = {
+      _id: id,
+      deleted: false
+    };
+    const data = await Role.findOne(find);
+    res.render("admin/pages/roles/edit", {
+      pageTitle: "Sửa nhóm quyền",
+      data: data
+    });
+  } catch (error) {
+    console.log(error);
+    //  res.redirect(`${systemConfig.prefixAdmin}/roles`)
   }
+};
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Role.updateOne({ _id: id }, req.body);
+    req.flash("success", "Cập nhật nhóm quyền thành công!");
+    const redirectUrl =
+    req.get("Referrer") || req.app.locals.prefixAdmin + "/products";
+    res.redirect(redirectUrl);
+  } catch (error) {
+    req.flash("error", "Cập nhật nhóm quyền thất bại !");
+  }
+};
