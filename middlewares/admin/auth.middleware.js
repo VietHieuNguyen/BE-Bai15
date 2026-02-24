26f02912d18d0984996fb6c5a6a36f637b7d0744
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-02-24 23:58:46 +0700

    B23 1 1. Thêm authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,20 +1,27 @@
-
-const systemConfig = require("../../config/system")
+const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
-module.exports.requireAuth = async (req,res, next)=>{
-  if(!req.cookies.token){
-    res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
-  }else{
+
+
+module.exports.requireAuth = async (req, res, next) => {
+  if (!req.cookies.token) {
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+  } else {
     const user = await Account.findOne({
       token: req.cookies.token,
-      deleted:false,
-      status: "active"
-    })
-    if(!user){
-    res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
+      deleted: false,
+      status: "active",
+    }).select("-password");
+    if (!user) {
+      res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+    } else {
+      const role = await Role.findOne({
+        _id: user.role_id,
+      }).select("title permissions");
+      res.locals.user = user;
+      res.locals.role = role;
+      next();
     }
-    next() 
   }
-  
-} 
+};
