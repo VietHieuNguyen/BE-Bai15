853e224ea90f199f76755fd1abf333cf638e2794
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-02-26 00:38:32 +0700

    B24 2 5. Trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,7 @@
 const productsHelper = require("../../helpers/products");
 const Product = require("../../models/product.model");
-
+const ProductCategory = require("../../models/product-category.model");
+const productsCategoryHelper = require("../../helpers/product-category")
 // [GET] /products
 
 module.exports.index = async (req, res) => {
@@ -29,5 +30,31 @@ module.exports.detail = async (req, res) => {
   res.render("client/pages/products/detail", {
     pageTitle: "Chi tiết sản phẩm",
     product: product,
+  });
+};
+
+// [GET] /products/detail:slugCategory
+module.exports.category = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    deleted: false,
+  });
+  
+  const listSubCategory = await productsCategoryHelper.getSubCategory(category.id);
+  
+  const listSubCategoryId = listSubCategory.map(item=>item.id)
+
+
+  const products = await Product.find({
+    product_category_id: {$in:[category.id, ...listSubCategoryId]},
+    deleted: false,
+    status: "active",
+  }).sort({ position: "desc" });
+  
+  const newProducts = productsHelper.priceNewProducts(products);
+
+  res.render("client/pages/products/index", {
+    pageTitle: category.title,
+    products: newProducts,
   });
 };
