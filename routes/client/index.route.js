1c55f8942f61a872c4500931b5e1aa8a2653423b
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-02-27 01:16:53 +0700

    B26 1 1.2 + 1.3 + 1.4

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,7 +2,7 @@ const productsRoutes = require("./product.route");
 const homeRoutes = require("./home.route")
 const searchRoutes = require("./search.route")
 const cartRoutes = require("./cart.route")
-
+const checkoutRoutes = require("./checkout.route")
 const categoryMiddleware = require("../../middlewares/client/category.middleware")
 const cartMiddleware = require("../../middlewares/client/cart.middleware")
 
@@ -19,6 +19,9 @@ module.exports = (app) => {
   app.use("/search", searchRoutes);
 
   app.use("/cart", cartRoutes);
+
+  app.use("/checkout", checkoutRoutes);
+
 
 
 };
