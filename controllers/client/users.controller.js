90595bbaec1d8d172a37c4007df44f450cdcef7d
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-03-04 23:42:26 +0700

    B30 1 1.3 Chức năng gửi yêu cầu

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -1,9 +1,28 @@
 const User = require("../../models/user.model")
+
+const userSocket = require("../../socket/client/user.socket")
 //[GET] /users/not-friend
 module.exports.notFriend = async (req, res)=>{
+  //Socket
+    userSocket(res);
+  //end socket
+
+
   const userId = res.locals.user.id;
+
+  const myUser = await User.findOne({
+    _id:userId
+  })
+  const requestFriends = myUser.requestFriends;
+  const acceptFriends = myUser.acceptFriends;
   const users = await User.find({
-    _id: {$ne: userId},
+    $and:[
+      {_id: {$ne: userId}},
+      {_id: {$nin: requestFriends}},
+      {_id: {$nin: acceptFriends}}
+    ],
+    
+    
     status: "active",
     deleted: false
   }).select("id avatar fullName")
